5865387452ffadf80c7c4f99ce4a41581b2fd04f
Ng Iris <43512172+Iris2000@users.noreply.github.com>
2019-04-18 10:38:30 +0800

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -17,22 +17,20 @@ public class Counter {
 
             System.out.print("Fish name: ");
             name = sc1.nextLine();
-            //fishObj[i].setFish(name);
 
             System.out.print("Fish price: ");
             price = sc1.nextDouble();
-            //fishObj[i].setPrice(price);
 
             System.out.print("fish weight: ");
             weight = sc1.nextDouble();
-            //fishObj[i].setWeight(weight);
             sc1.nextLine();
 
             fishObj[i] = new Fish(type, name, price, weight);
         }
 
         for(int i = 0; i < fishObj.length; i ++) {
-            System.out.print(fishObj);
+            System.out.println(fishObj[i]);
+            System.out.println("-------------------------------");
         }
     }
 }
